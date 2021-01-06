aa7b3931efc4fe22a0a49b24627c75fe119d00ad
Sonic <HungICT2407@gmail.com>
2021-01-06 10:30:16 +0700

    WIP

--- decoder/qr_bar_decoder/views.py
+++ decoder/qr_bar_decoder/views.py
@@ -69,13 +69,13 @@ def process_start_section(sid, data):
 	section_id = data
 	global out
 
-	print("Before exception")
+	# print("Before exception")
 	try:
 		os.mkdir(f"{settings.MEDIA_ROOT}/{section_id}")
 	except FileExistsError:  # Built-in exception.
 		print("FileExistsError: [WinError 183] Cannot create a file when that file already exists")
 
-	print("After exception...")
+	# print("After exception...")
 	out = cv2.VideoWriter(f"{settings.MEDIA_ROOT}/{section_id}/capture.mp4", 0x00000021, 24, (settings.VIDEO_WIDTH, settings.VIDEO_HEIGHT))  # https://stackoverflow.com/questions/49530857/python-opencv-video-format-play-in-browser
 	s = Section.objects.get(pk=section_id)
 	capture = Video(name=timezone.now(), url=f"{settings.DOMAIN_NAME}{settings.MEDIA_URL}{section_id}/capture.mp4", section=s)
@@ -117,12 +117,15 @@ def process_live_streaming_package(sid, data):
 
 	# image = cv2.imdecode(npimg, 1)
 	# image = cv2.imread("capture.png") # Deprecated method.
-	barcodes = pyzbar.decode(image, symbols=[pyzbar.ZBarSymbol.CODE128])
-	print(barcodes)
+	# print(image)
+	barcodes = pyzbar.decode(image)  # symbols=[pyzbar.ZBarSymbol.CODE128]
+	# print(barcodes)
 
 	csv = open("./barcodes.csv", "w")
 	found = set()
 	for barcode in barcodes:
+
+		print(barcode)
 
 		(x, y, w, h) = barcode.rect
 		cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
